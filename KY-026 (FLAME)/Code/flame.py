f7733ce02435b89e6677ba776e1f6cc9a30f4e54
Mario Silva <79481900+MerioUwUITT@users.noreply.github.com>
2022-10-13 00:34:56 -0700

    Update flame.py

--- KY-026 (FLAME)/Code/flame.py
+++ KY-026 (FLAME)/Code/flame.py
@@ -1,9 +1,9 @@
-from machine import Pin
-import time
+from machine import Pin#pin lib
+import time#time lib
 
-flame = Pin(0,Pin.IN)
+flame = Pin(0,Pin.IN)#analog input
     
-while True:
-    if flame.value() == 1:
-        print("fuego!")
-        time.sleep(1)
+while True:#loop 
+    if flame.value() == 0:#en analog input, el valor default es 1, al detectar fuego se vuelve 0
+        print("fuego!")#alerta en terminal
+        time.sleep(1)#sleep de 1 segundo para no saturar la terminal
